db3cc8048b5893498a2ec72fdcf2d665f9ca8954
Vinicio <viniciofernandes@outlook.com>
2025-04-12 01:12:27 -0300

    Mudanças Responsivo

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ for (const brAccordion of document.querySelectorAll(".br-accordion")) {
     Object.create(new core.BRAccordion("br-accordion", brAccordion))
   );
 }
+
+const headerList = [];
+for (const brHeader of window.document.querySelectorAll(".br-header")) {
+  headerList.push(new core.BRHeader("br-header", brHeader));
+}
+
+const listFooter = [];
+for (const brFooter of window.document.querySelectorAll(".br-footer")) {
+  listFooter.push(new core.BRFooter("br-footer", brFooter));
+}
