61382095d105b94e0c3b60f0582f428c2102e86a
Vinicio <viniciofernandes@outlook.com>
2025-04-12 02:07:22 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const accordionList = [];
-for (const brAccordion of document.querySelectorAll(".br-accordion")) {
-  accordionList.push(
-    Object.create(new core.BRAccordion("br-accordion", brAccordion))
-  );
+// Modularizar criação de componentes
+function initComponents(selector, ComponentClass) {
+  const elements = document.querySelectorAll(selector);
+  return elements.length
+    ? Array.from(elements).map((el) => new ComponentClass(selector, el))
+    : [];
 }
 
-const headerList = [];
-for (const brHeader of window.document.querySelectorAll(".br-header")) {
-  headerList.push(new core.BRHeader("br-header", brHeader));
-}
-
-const listFooter = [];
-for (const brFooter of window.document.querySelectorAll(".br-footer")) {
-  listFooter.push(new core.BRFooter("br-footer", brFooter));
-}
+// Uso
+const accordions = initComponents(".br-accordion", core.BRAccordion);
+const headers = initComponents(".br-header", core.BRHeader);
+const footers = initComponents(".br-footer", core.BRFooter);
